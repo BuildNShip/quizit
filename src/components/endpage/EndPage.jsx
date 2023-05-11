dd51a1c84c7324b752f9fe4f070b2da59a0763f8
AswinAsok <aswinasokofficial@gmail.com>
2023-05-11 11:27:33 +0530

    master:logic-changes

--- src/components/endpage/EndPage.jsx
+++ src/components/endpage/EndPage.jsx
@@ -7,6 +7,7 @@ const EndPage = ({ endPageText }) => {
     const { name } = useParams();
     const userKey = sessionStorage.getItem("userKey");
     const [endScreen, setEndScreen] = useState({});
+    const [responseType, setResponseType] = useState();
 
     useEffect(() => {
         console.log(localStorage.getItem("userKey"));
@@ -20,6 +21,18 @@ const EndPage = ({ endPageText }) => {
             .then(response => {
                 setEndScreen(response.data.response);
                 console.log(response.data);
+                if (!response.data.response.noOfAttempts) {
+                    setResponseType(0);
+                } else if (
+                    !response.data.response.answerLog ||
+                    response.data.response.answerLog.length === 0
+                ) {
+                    setResponseType(1);
+                } else if (!response.data.response.answerLog[0].correctAnswer) {
+                    setResponseType(2);
+                } else {
+                    setResponseType(3);
+                }
             })
             .catch(error => {
                 console.log(error);
@@ -29,54 +42,77 @@ const EndPage = ({ endPageText }) => {
     return (
         <div className={styles.background_container}>
             <div className={styles.first_view_container}>
-                <div className={styles.first_view}>
-                    <p className={styles.first_view_texts}>
-                        {name && (
-                            <img
-                                src={`${
-                                    import.meta.env.VITE_BACKEND_URL
-                                }quizit/v1/get-logo/${name}/`}
-                                alt="Logo of the quiz"
-                                className={styles.first_view_image}
-                            />
-                        )}
-                        <p className={styles.fv_heading}>{endPageText}</p>
-                        <p className={styles.fv_tagline}>
-                            Thanks for taking our quiz! We hope you enjoyed it. We
-                            will be back with more quizzes soon. Stay tuned!
+                {responseType === 0 && (
+                    <div className={styles.first_view}>
+                        <p className={styles.first_view_texts}>
+                            {name && (
+                                <img
+                                    src={`${
+                                        import.meta.env.VITE_BACKEND_URL
+                                    }quizit/v1/get-logo/${name}/`}
+                                    alt="Logo of the quiz"
+                                    className={styles.first_view_image}
+                                />
+                            )}
+                            <p className={styles.fv_heading}>{endPageText}</p>
+                            <p className={styles.fv_tagline}>
+                                Thanks for taking our quiz! We hope you enjoyed
+                                it. We will be back with more quizzes soon. Stay
+                                tuned!
+                            </p>
                         </p>
-                        <div className={styles.results_container}>
-                            <div className={styles.results}>
-                                <div className={styles.result}>
-                                    <p className={styles.count}>
-                                        {endScreen.noOfAttempts}
-                                    </p>
-                                    <p className={styles.count_text}>
-                                        Questions Answered
-                                    </p>
-                                </div>
-                                <div className={styles.result}>
-                                    <p className={styles.count}>
-                                        {" "}
-                                        {endScreen.noOfCorrects}
-                                    </p>
-                                    <p className={styles.noOfCorrects}>
-                                        Correct Answers
-                                    </p>
-                                </div>
-                                <div className={styles.result}>
-                                    <p className={styles.count}>
-                                        {endScreen.noOfAttempts -
-                                            endScreen.noOfCorrects}
-                                    </p>
-                                    <p className={styles.count_text}>
-                                        Wrong Answers
-                                    </p>
+                    </div>
+                )}
+                {responseType == 1 && (
+                    <div className={styles.first_view}>
+                        <p className={styles.first_view_texts}>
+                            {name && (
+                                <img
+                                    src={`${
+                                        import.meta.env.VITE_BACKEND_URL
+                                    }quizit/v1/get-logo/${name}/`}
+                                    alt="Logo of the quiz"
+                                    className={styles.first_view_image}
+                                />
+                            )}
+                            <p className={styles.fv_heading}>
+                                {" "}
+                                <span>{name}</span> - Result
+                            </p>
+
+                            <div className={styles.results_container}>
+                                <div className={styles.results}>
+                                    <div className={styles.result}>
+                                        <p className={styles.count}>
+                                            {endScreen.noOfAttempts}
+                                        </p>
+                                        <p className={styles.count_text}>
+                                            Questions Answered
+                                        </p>
+                                    </div>
+                                    <div className={styles.result}>
+                                        <p className={styles.count}>
+                                            {" "}
+                                            {endScreen.noOfCorrects}
+                                        </p>
+                                        <p className={styles.noOfCorrects}>
+                                            Correct Answers
+                                        </p>
+                                    </div>
+                                    <div className={styles.result}>
+                                        <p className={styles.count}>
+                                            {endScreen.noOfAttempts -
+                                                endScreen.noOfCorrects}
+                                        </p>
+                                        <p className={styles.count_text}>
+                                            Wrong Answers
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </p>
-                </div>
+                        </p>
+                    </div>
+                )}
             </div>
         </div>
     );
